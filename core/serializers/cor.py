99358c2bcc003c47bb8be3aeca153c79ba381c8e
nathaly.adao <nathalycarolineadaoo@gmail.com>
2025-04-22 14:49:43 -0300

    feat: criação da api Cor

---
+++ core/serializers/cor.py
@@ -0,0 +1,10 @@
+from core.models.cor import Cor
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Cor
+
+
+class CorSerializer(ModelSerializer):
+    class Meta:
+        model = Cor
+        fields = "__all__"
